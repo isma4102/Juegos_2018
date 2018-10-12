5ae579b2ce60191d3810b95812d6630f58a52664
isma4102 <santoscastelvi2014@gmail.com>
2018-10-12 15:06:02 -0300

    Jugador principal y enemigo

---
+++ Assets/Scripts/Player/EnemigoMovimiento.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemigoMovimiento : MonoBehaviour {
+
+    Transform jugador; //Guarda la posicion del jugador
+    NavMeshAgent IA;
+
+    private void Awake()
+    {
+        jugador = GameObject.FindGameObjectWithTag("Player").transform;
+        IA = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        IA.SetDestination(jugador.position);
+    }
+}
